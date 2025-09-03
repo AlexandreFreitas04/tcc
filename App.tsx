37a2688230d72f5c41ece3a2a9a46901f2ef9a19
Alexandre <alexandre.freitas2011@hotmail.com>
2025-09-03 00:12:19 -0300

    importando as dependencias e criando a constante do stack

--- App.tsx
+++ App.tsx
@@ -1,6 +1,10 @@
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
+import {Login, Home, Register} from './screens'
+import { NavigationContainer } from '@react-navigation/native';
+import { createNativeStackNavigator } from '@react-navigation/native-stack';
 
+const Stack = createNativeStackNavigator ();
 
 export default function App() {
   return (
