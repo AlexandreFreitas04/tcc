5719eeeb3766d257e00f6f505aae6b0c512a7c34
Alexandre <alexandre.freitas2011@hotmail.com>
2025-09-02 23:58:51 -0300

    criando telas iniciais

--- screens/Register.tsx
+++ screens/Register.tsx
@@ -1,8 +1,7 @@
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
 
-
-export default function App() {
+export default function Register() {
   return (
     <View style={styles.container}>
 
