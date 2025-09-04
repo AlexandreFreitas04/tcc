3c4946bd171853120e2c4025799634e0c8a979db
Alexandre <alexandre.freitas2011@hotmail.com>
2025-09-04 17:53:47 -0300

    criando a estrutura do formulario de login

--- screens/Login.tsx
+++ screens/Login.tsx
@@ -1,10 +1,32 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, TouchableOpacity, View } from 'react-native';
+import { useState } from 'react';
+import { useNavigation } from '@react-navigation/native';
 
 export default function Login() {
+  const [email, setEmail] = useState('');
+  const [senha, setSenha] = useState('');
+
+  const navigation = useNavigation()
+
+
   return (
     <View style={styles.container}>
        <Text> Tela Login</Text>
+
+       <TextInput 
+       placeholder='E-mail'
+       onChangeText = {texto => setEmail(texto)}
+       />
+
+       <TextInput 
+       placeholder = 'Senha'
+       onChangeText = {texto => setEmail (texto)}
+       />
+
+       <TouchableOpacity> 
+        <text>Entrar</text>
+       </TouchableOpacity>
     </View>
   );
 }
